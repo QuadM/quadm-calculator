0d2afc211449c290501343eca78d7088c3a3ff7a
QuadM <mahmoudhafez1511@gmail.com>
2022-05-18 02:50:29 +0200

    added node server

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+var path = require("path");
+var public = path.join(__dirname, "public");
+
 const app = express();
+const port = process.env.PORT || 4000;
 
-app.use(express.static("website"));
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => console.log("listening to port ", PORT, "..."));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use("/", express.static(public));
+app.listen(port, () => console.log(`listening to port : ${port}`));
