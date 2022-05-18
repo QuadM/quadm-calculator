3e426f7099e69f774949f91670a9b6b89295fec8
QuadM <mahmoudhafez1511@gmail.com>
2022-05-18 02:17:57 +0200

    added node server

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("website"));
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log("listening to port ", PORT, "..."));
